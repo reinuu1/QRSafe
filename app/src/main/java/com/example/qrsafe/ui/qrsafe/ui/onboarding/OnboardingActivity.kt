cdb7552df11e2366d61b4058dad00d9a9d6726e2
Dinu Andrei <andrei_marian.dinu@stud.fils.upb.ro>
2025-11-23 12:42:02 +0200

    Lucru la ecran incep+bara si animatiile de baza

---
+++ app/src/main/java/com/example/qrsafe/ui/qrsafe/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,27 @@
+package com.example.qrsafe.ui.onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.example.qrsafe.MainActivity
+
+
+class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            finish()
+        }
+    }
+}
